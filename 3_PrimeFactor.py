6e4d0d23772982bb05b32612a04ea59e0e929b31
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-07-08 20:41:43 +0100

    3_PrimeFactor.py

--- 3_PrimeFactor.py
+++ 3_PrimeFactor.py
@@ -1,11 +1,15 @@
+# Prompt: The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
+
 primes = []
 y = 2
 primefound = False
+
 def primefactor(x):
+    # recursively divides input by prime factor until input = 1 
     if x != 1:
-        for i in range(2,x + 1):
-            print(x,i)
+        for i in range(2, x + 1):
             if x % i == 0:
+                # if x is divisible by 1, the remainder will be 0
                 primes.append(i)
                 x = int(x/i)
                 primefactor(x)
@@ -13,8 +17,5 @@ def primefactor(x):
     else:
         return
 
-
 primefactor(600851475143)
 print(primes)
-
-
